fd125a8984f8aef39188ed8b362e66263471d558
OptimusV7 <manstill09@outlook.com>
2022-02-22 18:04:47 +0300

    Added content and route to admin page

---
+++ Hotel Core System/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel_Core_System.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/Home/index.cshtml");
+        }
+    }
+}
