e2d76fba9c9bdc28632c30f29a8450499f17a936
OptimusV7 <manstill09@outlook.com>
2022-02-22 19:58:33 +0300

    changes in all styling files added image in login page

---
+++ Hotel Core System/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel_Core_System.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult index()
+        {
+            return View("~/Views/Auth/AdminLogin.cshtml");
+        }
+    }
+}
