ff282cf3d4671527eacccde7c257f3727fa9171d
OptimusV7 <manstill09@outlook.com>
2022-03-25 17:59:13 +0300

    Added functions for roles users and permission

--- Hotel Core System/Controllers/AdminController.cs
+++ Hotel Core System/Controllers/AdminController.cs
@@ -8,5 +8,34 @@ namespace Hotel_Core_System.Controllers
         {
             return View("~/Views/Admin/Home/index.cshtml");
         }
+
+        public IActionResult UsersAll()
+        {
+            return View("~/Views/Admin/User/Index.cshtml");
+        }
+
+        public IActionResult UsersCreate()
+        {
+            return View("~/Views/Admin/User/Create.cshtml");
+        }
+        public IActionResult RolesAll()
+        {
+            return View("~/Views/Admin/Role/Index.cshtml");
+        }
+
+        public IActionResult RolesCreate()
+        {
+            return View("~/Views/Admin/Role/Create.cshtml");
+        }
+
+        public IActionResult PermissionAll()
+        {
+            return View("~/Views/Admin/Permission/Index.cshtml");
+        }
+
+        public IActionResult PermissionCreate()
+        {
+            return View("~/Views/Admin/Permission/Create.cshtml");
+        }
     }
 }
