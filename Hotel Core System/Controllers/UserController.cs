85f28fa5f6e65c897caf9f6997c9abcf8fd69df7
OptimusV7 <manstill09@outlook.com>
2022-03-28 16:21:33 +0300

    added fetch all users. waiting for fetch roles as per the users

--- Hotel Core System/Controllers/UserController.cs
+++ Hotel Core System/Controllers/UserController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel_Core_System.Models;
+using Hotel_Core_System.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Hotel_Core_System.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ApplicationDBContext _db;
+        UserManager<ApplicationUser> _userManager;
+
+        public UserController(ApplicationDBContext _context, UserManager<ApplicationUser> userManager)
+        {
+            _db = _context;
+            _userManager = userManager;
+        }
+
         public IActionResult UsersAll()
         {
-            return View("~/Views/Admin/User/Index.cshtml");
+            var userlist = (from user in _db.Users
+                            select new AccountVM
+                            {
+                                Id = user.Id,
+                                UserName = user.UserName,
+                                Name = user.Name,
+                                Email = user.Email,
+                                EmailConfirmed = user.EmailConfirmed,
+                                Phone = user.PhoneNumber
+                            }).ToList();
+
+            return View("~/Views/Admin/User/Index.cshtml", userlist);
         }
 
         public IActionResult UsersCreate()
@@ -32,5 +58,13 @@ namespace Hotel_Core_System.Controllers
         {
             return View("~/Views/Admin/Permission/Create.cshtml");
         }
+
+        public IActionResult GetUsers()
+        {
+            var userList = _userManager.Users;
+
+            return (IActionResult)userList;
+        }
+        
     }
 }
---
+++ Hotel Core System/Models/ViewModels/AccountVM.cs
@@ -0,0 +1,19 @@
+﻿namespace Hotel_Core_System.Models.ViewModels
+{
+    public class AccountVM
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Phone { get; set; }
+        
+        public bool EmailConfirmed { get; set; }
+        
+        public string RoleName { get; set; }
+    }
+}
