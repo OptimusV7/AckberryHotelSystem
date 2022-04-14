ac557e0b38f00c5a31cf4f94dbe56c524473705f
OptimusV7 <manstill09@outlook.com>
2022-04-14 11:17:02 +0300

    upadates in views

---
+++ Hotel Core System/Models/RoomImage.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel_Core_System.Models
+{
+    public class RoomImage
+    {
+        [Key]
+        public int ImageID { get; set; }
+        public int RoomNumber { get; set; }
+        public string ImageName { get; set; }
+        public string Caption { get; set; }
+    }
+
+}
---
+++ Hotel Core System/Models/ViewModels/RoomVM.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotel_Core_System.Models.ViewModels
+{
+    public class RoomVM
+    {
+        [Required, Display(Name = "Room Number")]
+        public string RoomNumber { get; set; }
+        [Required, Display(Name = "Room Type")]
+        public string RoomType { get; set; }
+        [Required, Display(Name = "Booking Price")]
+        public double BookingPrice { get; set; }
+        [Required, Display(Name = "Is Smoking Allowed")]
+        public bool IsSmokingAllowed { get; set; }
+        [Required, Display(Name = "Is Room Available")]
+        public bool IsRoomAvailable { get; set; }
+        
+        public IFormFile ImageFile { get; set; }
+    }
+}
